bd2ca5a47319b3e43de9a7d5574ae7d65f06c6e8
wenermao <1432914530@qq.com>
2017-04-14 20:28:11 +0800

    split1.py

--- split1.py
+++ split1.py
@@ -2,11 +2,11 @@ import re
 hand = open('spl.txt')
 need = list()
 for line in hand:
-    z = line.split(" ")[1]
-    print z
+    z = line.split(" ")[0]
     need.append(z)
 print need
 
 open("one.txt", 'wb').write(str(need))
 #this is i will delete
 #this is a branch
+#newone
